f7c8f2cbdd348c8a24022fae0e0bcc9c1a68c4ce
Rustam <rustam.mirzamagomedov@gomypartner.com>
2022-08-13 16:11:41 +0300

    add redirect to admin page

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  redirects: async () => {
+    return [
+      {
+        source: "/",
+        destination: "/admin",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
